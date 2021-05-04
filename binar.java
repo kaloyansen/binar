6d905e46b79c807d5ec5dc700dc1a311af327ba6
Kaloyan KRASTEV <83513233+kaloyansen@users.noreply.github.com>
2021-05-04 22:26:56 +0200

    Update binar.java

--- binar.java
+++ binar.java
@@ -6,8 +6,8 @@ code original - kaloyansen@gmail.com */
           for (int i = 1; i < x; i++) { this.y *= 2; }
           if (x == 0) { this.y = 1; }
           if (this.y <= 0) {
-               System.out.println("warning this calculation does not work for a value of y greater than thirty");
-               System.out.println("and so it will definitely not work for a value as high as " + x);
+               sop("warning this calculation does not work for a value of y greater than 62");
+               sop("and so it will definitely not work for a value as high as " + x);
           }
           this.x = x;
      }
@@ -15,18 +15,18 @@ code original - kaloyansen@gmail.com */
      static int toint(String s) {
           int i = 0;
           try { i = Integer.parseInt(s); }
-          catch (NumberFormatException e) { System.out.println("warning '" + s + "' is not a number"); }
+          catch (NumberFormatException e) { sop("warning '" + s + "' is not a number"); }
           return i;
      }
 
      static int oneintarg(String[] args) {
           int arle = args.length;
           if (arle == 0) {// no arguments detected will return zero
-               System.out.println("warning int argument expected the code set to default zero");
+               sop("warning int argument expected the code set to default zero");
                return 0;
           }
           if (arle > 1) { // too many arguments not fatal just warning
-               System.out.println("warning one int argument expected the code ignores any other");
+               sop("warning one int argument expected the code ignores any other");
           }
           return toint(args[0]);
      }
@@ -34,9 +34,8 @@ code original - kaloyansen@gmail.com */
      public static void main(String[] args) {
           int intarg = oneintarg(args);
           binar bin = new binar(intarg);
-          System.out.println("2 au " + bin.x + "-thème degré et " + bin.y);
+          sop("2 au " + bin.x + "-thème degré et " + bin.y);
      }
 
+     public static void sop(String s) { System.out.println(s); }
 }
-
-
